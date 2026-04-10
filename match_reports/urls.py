677343090c17dce7bd7730f28f3432b24cfff3ae
YemsAla <ylawal7@gmail.com>
2026-04-10 11:22:35 +0100

    Create app urls

---
+++ match_reports/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.ReportListView.as_view(), name='report_list'),
+    path('report/<int:pk>/', views.ReportDetailView.as_view(), name='report_detail'),
+]
