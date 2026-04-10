7eb09ac70a06fa5344ee7d1b88f160a1a357e597
YemsAla <ylawal7@gmail.com>
2026-04-10 10:52:13 +0100

    Add MatchReportForm and update views scaffold

--- match_reports/forms.py
+++ match_reports/forms.py
@@ -2,6 +2,21 @@ from django import forms
 from .models import MatchReport, Comment
 
 
+"""
+Form fields for adding a match report
+"""
+class MatchReportForm(forms.ModelForm):
+    class Meta:
+        model = MatchReport
+        fields = ['title', 'team_name', 'opponent_name', 'team_score', 'opponent_score', 'match_report', 'image', 'player_of_match', 'opponent_player_of_match', 'match_date', 'match_type']
+        widgets = {
+            'match_date': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+"""
+Comment form for adding comments to a match report
+"""
+        
 class commentForm(forms.ModelForm):
     class Meta:
         model = Comment
@@ -9,3 +24,4 @@ class commentForm(forms.ModelForm):
         widgets = {
             'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a comment...'}),
         }
+        
--- match_reports/views.py
+++ match_reports/views.py
@@ -6,9 +6,9 @@ from django import messages
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-from match_reports.forms import commentForm
+from match_reports.forms import MatchReportForm, commentForm
 
-from .models import MatchReport, Comment 
+from .models import MatchReport, Comment
 
 
 """
@@ -31,7 +31,7 @@ class ReportDetailView(DetailView):
     context_object_name = 'report'
     
     """
-    Override to include comments and comment form in the context.
+    Users can comment on an individual report, and the comments are displayed in reverse chronological order.
     """    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -39,18 +39,37 @@ class ReportDetailView(DetailView):
         context['comment_form'] = commentForm()
         return context
     
-   @login_required
-   def add_comment(request, pk):
-        report = get_object_or_404(MatchReport, pk=pk)
+""" 
+View for adding a comment to a match report. Only accessible to logged-in users.
+"""
+  
+@login_required
+def add_comment(request, pk):
+    report = get_object_or_404(MatchReport, pk=pk)
         
-        if request.method == 'POST':
-            form = commentForm(request.POST)
-            if form.is_valid():
-                comment = form.save(commit=False)
-                comment.user = request.user
-                comment.match_report = report
-                comment.save()
-                messages.success(request, 'Your comment has been added.')
-            else:
-                messages.error(request, 'There was an error with your comment. Please try again.')
-        return redirect('report-detail', pk=report.pk)
+    if request.method == 'POST':
+        form = commentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.match_report = report
+            comment.save()
+            messages.success(request, 'Your comment has been added.')
+        else:
+            messages.error(request, 'There was an error with your comment. Please try again.')
+    return redirect('report-detail', pk=report.pk)
+   
+
+"""
+View for creating a new match report. Only accessible to logged-in users.
+When a report is successfully created, a success message is displayed to the user.
+"""  
+class ReportCreateView(LoginRequiredMixin, CreateView):
+    model = MatchReport
+    form_class = MatchReportForm
+    template_name = 'match_reports/report_form.html'
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, 'Your match report has been successfully created.')
+        return super().form_valid(form)
