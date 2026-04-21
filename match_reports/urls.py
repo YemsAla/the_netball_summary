5edafe795880c2ee4f97f33a3167ede0423d72e2
YemsAla <ylawal7@gmail.com>
2026-04-21 18:43:08 +0100

    feat: separate homepage and match reports list pages

--- match_reports/urls.py
+++ match_reports/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path ('', views.home, name='home'),
-    path('', views.ReportListView.as_view(), name='report_list'),  
-    path('<int:pk>/', views.ReportDetailView.as_view(), name='report_detail'),
-    path('<int:pk>/comment/', views.add_comment, name='add-comment'),
-    path('create/', views.ReportCreateView.as_view(), name='report-create'),
+    path('reports/', views.ReportListView.as_view(), name='report_list'),  
+    path('reports/<int:pk>/', views.ReportDetailView.as_view(), name='report_detail'),
+    path('reports/<int:pk>/comment/', views.add_comment, name='add-comment'),
+    path('reports/create/', views.ReportCreateView.as_view(), name='report-create'),
 ]
