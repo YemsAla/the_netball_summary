3193115c69850de04a8be24f04812f416f2d4152
YemsAla <ylawal7@gmail.com>
2026-05-06 14:29:31 +0100

    feat: secure SECRET_KEY using environment variables
    - Install python-dotenv
    - Create .env file with SECRET_KEY and DEBUG variables
    - Add .env to .gitignore to prevent secret key being exposed
    - Update settings.py to load SECRET_KEY and DEBUG from environment
    - Update requirements.txt

--- the_netball_summary/settings.py
+++ the_netball_summary/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,10 +27,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z0-7e*98=ck)%b5!9v!&8tf4sz4x$xeooipyiovo)zjo(t4kq('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
