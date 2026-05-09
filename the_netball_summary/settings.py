4a5777b5864d35fc8b9956fd44468be56d315003
YemsAla <ylawal7@gmail.com>
2026-05-09 11:35:51 +0100

    fix: move cloudinary_storage before staticfiles in INSTALLED_APPS

--- the_netball_summary/settings.py
+++ the_netball_summary/settings.py
@@ -27,8 +27,8 @@ import cloudinary.api
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Media files (User-uploaded files)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -53,9 +53,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'cloudinary_storage',
-     'cloudinary',
+    'django.contrib.staticfiles',
+    'cloudinary',
     'match_reports',
 ]
 
