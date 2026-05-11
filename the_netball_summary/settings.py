c4e1eab08f90408d04fb373d9776c12d6946a592
YemsAla <ylawal7@gmail.com>
2026-05-11 13:04:50 +0100

    Temporarily allow iframes for mockup screenshot

--- the_netball_summary/settings.py
+++ the_netball_summary/settings.py
@@ -136,6 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+X_FRAME_OPTIONS = 'ALLOWALL'
+
 # Database
 DATABASES = {
     'default': dj_database_url.config(
