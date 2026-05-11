ad39f4d8a808e100b0da5f38c120afcb2c42c99c
YemsAla <ylawal7@gmail.com>
2026-05-11 13:09:51 +0100

    Remove temporary X_FRAME_OPTIONS setting

--- the_netball_summary/settings.py
+++ the_netball_summary/settings.py
@@ -136,8 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-X_FRAME_OPTIONS = 'ALLOWALL'
-
 # Database
 DATABASES = {
     'default': dj_database_url.config(
